e1a54ab80354e8cf67dddc59ca44269cfbbfedb2
pwlolk <p.j.olszak@gmail.com>
2018-11-14 22:50:55 +0100

    Whatever

--- Dzien01/dzien01_05.py
+++ Dzien01/dzien01_05.py
@@ -1,8 +1,7 @@
 imie = input("Jak masz na imię? ")
 imie = imie.strip(' ')
 ostatnia_litera_imienia = imie[-1:]
-if(ostatnia_litera_imienia == 'a'):
+if (ostatnia_litera_imienia == 'a'):
     print("Cześć, dziewczyno!")
 else:
     print("Cześć, chłopaku!")
-# coś tam

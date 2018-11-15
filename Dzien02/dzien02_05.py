25f9b0c8b543b96c19b84f4e13c7139e8651c874
pwlolk <p.j.olszak@gmail.com>
2018-11-15 16:51:36 +0100

    Zmiana numeru dnia

---
+++ Dzien02/dzien02_05.py
@@ -0,0 +1,7 @@
+imie = input("Jak masz na imię? ")
+imie = imie.strip(' ')
+ostatnia_litera_imienia = imie[-1:]
+if (ostatnia_litera_imienia == 'a'):
+    print("Cześć, dziewczyno!")
+else:
+    print("Cześć, chłopaku!")

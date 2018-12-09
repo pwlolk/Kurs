0c14fae1cdea1c11a5f8d2a90feb730946401698
pwlolk <p.j.olszak@gmail.com>
2018-12-09 18:27:12 +0100

    Changes

--- Dzien07/dzien07_01.py
+++ Dzien07/dzien07_01.py
@@ -11,26 +11,12 @@ def intro_file_as_csv():
         print("Podaj prawidłową nazwę pliku.\n")
         intro_file_as_csv()
 
-# def intro_file_as_csv():
-#     csv_file = input("Wskaż plik do przeanalizowania: ")
-#     try:
-#         with open(csv_file, "r+", newline='') as csv_data_file:
-#             reader = csv.reader(csv_data_file)
-#             return reader
-#     except:
-#         print("Podaj prawidłową nazwę pliku.\n")
-#         intro_file_as_csv()
+def main_menu():
+    print("\nOpcje:\n"
+          "[1] Wyszukanie statków podobnych [kryteria OLPA]\n"
+          "[2] Wyszukanie statków o zadanych parametrach\n")
 
-# def intro_file_as_txt():
-#     csv_file = input("Wskaż plik do przeanalizowania: ")
-#     try:
-#          with open(csv_file, "r+") as txt_file:
-#             txt_file.seek(0)
-#             single_string = str(txt_file.read())
-#             return single_string
-#     except:
-#         print("Podaj prawidłową nazwę pliku.\n")
-#         intro_file_as_txt()
+# def decision_switch():
 
 #Teoretycznie operacja na konkretnej wielkości z tabeli powinna być tożsama z konkretnym indexem w row.
 #Tak można przyporzadkować funkcję.
@@ -48,8 +34,19 @@ def dwt_summary(csv_file):
                 dwt_summary = dwt_summary + float(row[9])
         return dwt_summary
 
+def legth_criteria(csv_file):
+    with open(csv_file, "r+") as csv_data_file:
+        reader = csv.reader(csv_data_file) #handler
+        for i,row in enumerate(reader):
+            if (i == 0):
+                continue
+            if (float(row[4]) > 85.00 and float(row[4]) < 100.00):
+                print(row[0], row[1])
+
 csv_file = intro_file_as_csv()
-dwt = dwt_summary(csv_file)
-print(format(dwt, ".2f"))
+main_menu()
+# dwt = dwt_summary(csv_file)
+# print(format(dwt, ".2f"))
+# legth_criteria(csv_file)
 
 #problemy: czym jest reader, jak podejrzeć pickle, jak odnieść się do row w readerze
